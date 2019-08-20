c5ce6935e053a74305726426385f28b2be5fc4f1
liangxin1300 <XLiang@suse.com>
2019-08-20 14:29:46 +0800

    dev: bootstrap: manage qnetd node

--- crmsh/bootstrap.py
+++ crmsh/bootstrap.py
@@ -1499,6 +1499,19 @@ Configure Administration IP Address:
     wait_for_resource("Configuring virtual IP ({})".format(adminaddr), "admin-ip")
 
 
+def init_qdevice():
+    if _context.qdevice:
+        try:
+            status("""
+Enable corosync-qnetd.service""")
+            _context.qdevice.enable()
+            status("""
+Starting corosync-qnetd.service""")
+            _context.qdevice.start()
+        except ValueError as err:
+            error(err)
+
+
 def init():
     """
     Basic init
@@ -2153,6 +2166,7 @@ def bootstrap_init(cluster_name="hacluster", ui_context=None, nic=None, ocfs2_de
         if template == 'ocfs2':
             init_vgfs()
         init_admin()
+        init_qdevice()
 
     status("Done (log saved to %s)" % (LOG_FILE))
 
